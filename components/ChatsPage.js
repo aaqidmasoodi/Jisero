f9b06d0ee298b3cbf841387ecbc3632cacdc2915
aaqidmasoodi <aaqidmasoodi@icloud.com>
2025-09-07 22:34:31 +0100

    Fix JSX syntax error - remove extra closing div tag

--- components/ChatsPage.js
+++ components/ChatsPage.js
@@ -197,7 +197,6 @@ const ChatsPage = memo(({ chats, onSelectChat, currentChatId, onArchive, onDelet
           ))
         )}
       </div>
-      </div>
       {/* Contact Profile Modal */}
       {showContactProfile && selectedContact && (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center p-4">
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jisero-v1.0.9';
+const CACHE_NAME = 'jisero-v1.1.0';
 const urlsToCache = [
   '/',
   '/index.html',
