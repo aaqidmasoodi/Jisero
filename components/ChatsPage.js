2441d9f52908ab05b2da6e058bc761ccb2b72882
aaqidmasoodi <aaqidmasoodi@icloud.com>
2025-09-07 22:31:08 +0100

    Fix missing closing div tag in ChatsPage

--- components/ChatsPage.js
+++ components/ChatsPage.js
@@ -197,6 +197,7 @@ const ChatsPage = memo(({ chats, onSelectChat, currentChatId, onArchive, onDelet
           ))
         )}
       </div>
+      </div>
       {/* Contact Profile Modal */}
       {showContactProfile && selectedContact && (
         <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex items-center justify-center p-4">
